432c516b15089846fb30dd93735358d9db7d309b
Pavel Iakushin <jetpilot412@gmail.com>
2022-01-18 14:05:06 +0300

    implement fungibles traits

--- pallets/deip_assets/src/impl_fungibles.rs
+++ pallets/deip_assets/src/impl_fungibles.rs
@@ -11,20 +11,20 @@ use sp_runtime::DispatchError;
 use crate::{Config, Pallet};
 
 impl<T: Config> Inspect<<T as SystemConfig>::AccountId> for Pallet<T> {
-    type AssetId = <T as Config>::AssetId;
+    type AssetId = <T as pallet_assets::Config>::AssetId;
 
     type Balance = <T as pallet_assets::Config>::Balance;
 
     fn total_issuance(asset: Self::AssetId) -> Self::Balance {
-        todo!()
+        pallet_assets::Pallet::<T>::total_issuance(asset)
     }
 
     fn minimum_balance(asset: Self::AssetId) -> Self::Balance {
-        todo!()
+        pallet_assets::Pallet::<T>::minimum_balance(asset)
     }
 
     fn balance(asset: Self::AssetId, who: &<T as SystemConfig>::AccountId) -> Self::Balance {
-        todo!()
+        pallet_assets::Pallet::<T>::balance(asset, who)
     }
 
     fn reducible_balance(
@@ -32,7 +32,7 @@ impl<T: Config> Inspect<<T as SystemConfig>::AccountId> for Pallet<T> {
         who: &<T as SystemConfig>::AccountId,
         keep_alive: bool,
     ) -> Self::Balance {
-        todo!()
+        pallet_assets::Pallet::<T>::reducible_balance(asset, who, keep_alive)
     }
 
     fn can_deposit(
@@ -40,7 +40,7 @@ impl<T: Config> Inspect<<T as SystemConfig>::AccountId> for Pallet<T> {
         who: &<T as SystemConfig>::AccountId,
         amount: Self::Balance,
     ) -> DepositConsequence {
-        todo!()
+        pallet_assets::Pallet::<T>::can_deposit(asset, who, amount)
     }
 
     fn can_withdraw(
@@ -48,7 +48,7 @@ impl<T: Config> Inspect<<T as SystemConfig>::AccountId> for Pallet<T> {
         who: &<T as SystemConfig>::AccountId,
         amount: Self::Balance,
     ) -> WithdrawConsequence<Self::Balance> {
-        todo!()
+        pallet_assets::Pallet::<T>::can_withdraw(asset, who, amount)
     }
 }
 
@@ -58,7 +58,7 @@ impl<T: Config> Mutate<<T as SystemConfig>::AccountId> for Pallet<T> {
         who: &<T as SystemConfig>::AccountId,
         amount: Self::Balance,
     ) -> DispatchResult {
-        todo!()
+        pallet_assets::Pallet::<T>::mint_into(asset, who, amount)
     }
 
     fn burn_from(
@@ -66,6 +66,6 @@ impl<T: Config> Mutate<<T as SystemConfig>::AccountId> for Pallet<T> {
         who: &<T as SystemConfig>::AccountId,
         amount: Self::Balance,
     ) -> Result<Self::Balance, DispatchError> {
-        todo!()
+        pallet_assets::Pallet::<T>::burn_from(asset, who, amount)
     }
 }
