00b8d0bee62b111b305a797730cd7ed4a364eba2
Pavel Iakushin <jetpilot412@gmail.com>
2022-01-19 15:36:50 +0300

    add and plug in blank pallet uniques wrapper

--- pallets/deip_uniques/src/lib.rs
+++ pallets/deip_uniques/src/lib.rs
@@ -1,4 +1,14 @@
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
+pub use pallet::{Config, Pallet};
 pub use pallet_uniques;
+
+#[frame_support::pallet]
+pub mod pallet {
+    #[pallet::config]
+    pub trait Config: frame_system::Config {}
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+}
