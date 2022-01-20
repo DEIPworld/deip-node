82402a88ca5eff4577f14426e0f98120ae15e33c
Pavel Iakushin <jetpilot412@gmail.com>
2022-01-20 13:45:19 +0300

    add and plug in blank config, genesis config

--- pallets/deip_uniques/src/lib.rs
+++ pallets/deip_uniques/src/lib.rs
@@ -1,7 +1,7 @@
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub use pallet::{Config, Pallet};
+pub use pallet::*;
 pub use pallet_uniques;
 
 #[frame_support::pallet]
@@ -11,4 +11,24 @@ pub mod pallet {
 
     #[pallet::pallet]
     pub struct Pallet<T>(_);
+
+    #[pallet::genesis_config]
+    pub struct GenesisConfig<T: Config> {}
+
+    #[pallet::genesis_build]
+    impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
+        fn build(&self) {
+            Self {}
+        }
+    }
+
+    #[cfg(feature = "std")]
+    impl<T: Config> Default for GenesisConfig<T> {
+        fn default() -> Self {
+            Self {}
+        }
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {}
 }
