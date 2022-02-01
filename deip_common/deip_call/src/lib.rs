97e41bffc5cada08a22d9f82974c3f6ead82cbcc
yaro <0623forbidden@gmail.com>
2022-02-01 21:06:07 +0300

    (deip-uniques): Review

--- deip_common/deip_call/src/lib.rs
+++ deip_common/deip_call/src/lib.rs
@@ -62,8 +62,7 @@ impl Serialize for WrappedCall<Call> {
             Call::Assets(deip_assets_call) =>
                 Self::serialize_deip_assets_call(deip_assets_call, serializer),
 
-            Call::Uniques(..) => todo!(),
-
+            Call::Uniques(..) |
             Call::System(_) |
             Call::DeipPortal(_) |
             Call::Timestamp(_) |
