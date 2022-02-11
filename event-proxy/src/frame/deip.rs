bafe30048475d41a6fbaafaff33bd85e65e0e02b
Pavel Iakushin <jetpilot412@gmail.com>
2022-02-11 15:25:14 +0300

    finish events decoding

--- event-proxy/src/frame/deip.rs
+++ event-proxy/src/frame/deip.rs
@@ -1,12 +1,19 @@
-use codec::Decode;
 use frame_support::Parameter;
 use frame_system::Config;
 use sp_runtime::traits::Member;
-use sp_std::prelude::*;
 
 use serde::{
     ser::{SerializeStruct, Serializer},
     Serialize,
+};
+
+use crate::appchain_deip::deip::events::{
+    ContractAgreementAccepted, ContractAgreementCreated, ContractAgreementFinalized,
+    ContractAgreementRejected, DomainAdded, Invested, NdaAccessRequestCreated,
+    NdaAccessRequestFulfilled, NdaAccessRequestRejected, NdaCreated, ProjectContnetCreated,
+    ProjectCreated, ProjectRemoved, ProjectUpdated, ReviewCreated, ReviewUpvoted,
+    SimpleCrowdfundingActivated, SimpleCrowdfundingCreated, SimpleCrowdfundingExpired,
+    SimpleCrowdfundingFinished,
 };
 
 pub trait Deip: Config {
@@ -26,9 +33,7 @@ pub trait Deip: Config {
 
 const ACCOUNT_ID_KEY: &str = "account_id";
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct ProjectCreatedEvent<T: Deip>(T::AccountId, T::Project);
-impl<T: Deip> Serialize for ProjectCreatedEvent<T> {
+impl Serialize for ProjectCreated {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -40,9 +45,7 @@ impl<T: Deip> Serialize for ProjectCreatedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct ProjectRemovedEvent<T: Deip>(T::AccountId, T::Project);
-impl<T: Deip> Serialize for ProjectRemovedEvent<T> {
+impl Serialize for ProjectRemoved {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -54,9 +57,7 @@ impl<T: Deip> Serialize for ProjectRemovedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct ProjectUpdatedEvent<T: Deip>(T::AccountId, T::ProjectId);
-impl<T: Deip> Serialize for ProjectUpdatedEvent<T> {
+impl Serialize for ProjectUpdated {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -68,9 +69,7 @@ impl<T: Deip> Serialize for ProjectUpdatedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct ProjectContentCreatedEvent<T: Deip>(T::AccountId, T::ProjectContentId);
-impl<T: Deip> Serialize for ProjectContentCreatedEvent<T> {
+impl Serialize for ProjectContnetCreated {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -82,9 +81,7 @@ impl<T: Deip> Serialize for ProjectContentCreatedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct NdaCreatedEvent<T: Deip>(T::AccountId, T::NdaId);
-impl<T: Deip> Serialize for NdaCreatedEvent<T> {
+impl Serialize for NdaCreated {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -96,9 +93,7 @@ impl<T: Deip> Serialize for NdaCreatedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct NdaAccessRequestCreatedEvent<T: Deip>(T::AccountId, T::NdaAccessRequestId);
-impl<T: Deip> Serialize for NdaAccessRequestCreatedEvent<T> {
+impl Serialize for NdaAccessRequestCreated {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -110,9 +105,7 @@ impl<T: Deip> Serialize for NdaAccessRequestCreatedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct NdaAccessRequestFulfilledEvent<T: Deip>(T::AccountId, T::NdaAccessRequestId);
-impl<T: Deip> Serialize for NdaAccessRequestFulfilledEvent<T> {
+impl Serialize for NdaAccessRequestFulfilled {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -124,9 +117,7 @@ impl<T: Deip> Serialize for NdaAccessRequestFulfilledEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct NdaAccessRequestRejectedEvent<T: Deip>(T::AccountId, T::NdaAccessRequestId);
-impl<T: Deip> Serialize for NdaAccessRequestRejectedEvent<T> {
+impl Serialize for NdaAccessRequestRejected {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -138,9 +129,7 @@ impl<T: Deip> Serialize for NdaAccessRequestRejectedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct DomainAddedEvent<T: Deip>(T::AccountId, T::DomainId);
-impl<T: Deip> Serialize for DomainAddedEvent<T> {
+impl Serialize for DomainAdded {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -152,9 +141,7 @@ impl<T: Deip> Serialize for DomainAddedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct ReviewCreatedEvent<T: Deip>(T::AccountId, T::Review);
-impl<T: Deip> Serialize for ReviewCreatedEvent<T> {
+impl Serialize for ReviewCreated {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -166,9 +153,7 @@ impl<T: Deip> Serialize for ReviewCreatedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct ReviewUpvotedEvent<T: Deip>(T::ReviewId, T::AccountId, T::DomainId);
-impl<T: Deip> Serialize for ReviewUpvotedEvent<T> {
+impl Serialize for ReviewUpvoted {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -183,9 +168,7 @@ impl<T: Deip> Serialize for ReviewUpvotedEvent<T> {
 
 const INVESTMENT_ID_KEY: &str = "investment_id";
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct SimpleCrowdfundingCreatedEvent<T: Deip>(T::InvestmentId);
-impl<T: Deip> Serialize for SimpleCrowdfundingCreatedEvent<T> {
+impl Serialize for SimpleCrowdfundingCreated {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -196,9 +179,7 @@ impl<T: Deip> Serialize for SimpleCrowdfundingCreatedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct SimpleCrowdfundingActivatedEvent<T: Deip>(T::InvestmentId);
-impl<T: Deip> Serialize for SimpleCrowdfundingActivatedEvent<T> {
+impl Serialize for SimpleCrowdfundingActivated {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -209,9 +190,7 @@ impl<T: Deip> Serialize for SimpleCrowdfundingActivatedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct SimpleCrowdfundingFinishedEvent<T: Deip>(T::InvestmentId);
-impl<T: Deip> Serialize for SimpleCrowdfundingFinishedEvent<T> {
+impl Serialize for SimpleCrowdfundingFinished {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -222,9 +201,7 @@ impl<T: Deip> Serialize for SimpleCrowdfundingFinishedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct SimpleCrowdfundingExpiredEvent<T: Deip>(T::InvestmentId);
-impl<T: Deip> Serialize for SimpleCrowdfundingExpiredEvent<T> {
+impl Serialize for SimpleCrowdfundingExpired {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -235,9 +212,7 @@ impl<T: Deip> Serialize for SimpleCrowdfundingExpiredEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct InvestedEvent<T: Deip>(T::InvestmentId, T::AccountId);
-impl<T: Deip> Serialize for InvestedEvent<T> {
+impl Serialize for Invested {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -251,9 +226,7 @@ impl<T: Deip> Serialize for InvestedEvent<T> {
 
 const CONTRACTAGREEMENT_ID_KEY: &str = "contractagreement_id";
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct ContractAgreementCreatedEvent<T: Deip>(T::ContractAgreementId);
-impl<T: Deip> Serialize for ContractAgreementCreatedEvent<T> {
+impl Serialize for ContractAgreementCreated {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -264,9 +237,7 @@ impl<T: Deip> Serialize for ContractAgreementCreatedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct ContractAgreementAcceptedEvent<T: Deip>(T::ContractAgreementId, T::AccountId);
-impl<T: Deip> Serialize for ContractAgreementAcceptedEvent<T> {
+impl Serialize for ContractAgreementAccepted {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -278,9 +249,7 @@ impl<T: Deip> Serialize for ContractAgreementAcceptedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct ContractAgreementFinalizedEvent<T: Deip>(T::ContractAgreementId);
-impl<T: Deip> Serialize for ContractAgreementFinalizedEvent<T> {
+impl Serialize for ContractAgreementFinalized {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
@@ -291,9 +260,7 @@ impl<T: Deip> Serialize for ContractAgreementFinalizedEvent<T> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Decode)]
-pub struct ContractAgreementRejectedEvent<T: Deip>(T::ContractAgreementId, T::AccountId);
-impl<T: Deip> Serialize for ContractAgreementRejectedEvent<T> {
+impl Serialize for ContractAgreementRejected {
     fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
     where
         S: Serializer,
