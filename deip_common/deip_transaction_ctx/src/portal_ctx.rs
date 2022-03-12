d6f155fcd706d6841c2025a5e3944967ba3ab23f
yaro <0623forbidden@gmail.com>
2022-03-12 14:15:54 +0300

    release/v0.1-beta.5

--- deip_common/deip_transaction_ctx/src/portal_ctx.rs
+++ deip_common/deip_transaction_ctx/src/portal_ctx.rs
@@ -2,13 +2,9 @@ use super::{TransactionCtxId, TransactionCtxT};
 
 use scale_info::TypeInfo;
 use sp_runtime::{traits::Dispatchable, DispatchResultWithInfo};
-use sp_std::prelude::Vec;
 
 pub trait PortalCtxT<LocalCall>: TransactionCtxT + TypeInfo {
     type PortalId;
-    type Extrinsic;
-    type Error;
-    type Delegate;
 
     fn portal_id(ctx: &TransactionCtxId<Self>) -> Self::PortalId;
 
@@ -19,22 +15,6 @@ pub trait PortalCtxT<LocalCall>: TransactionCtxT + TypeInfo {
         call: D,
         origin: D::Origin,
     ) -> DispatchResultWithInfo<D::PostInfo>;
-
-    /// Schedule extrinsic to be dispatched within Portal context.
-    fn schedule_extrinsic(
-        &self,
-        xt: Self::Extrinsic,
-        delegate: Self::Delegate,
-    ) -> Result<(), Self::Error>;
-
-    fn submit_scheduled(at: Self::BlockNumber) -> Result<Vec<()>, ()>;
-
-    fn dispatch_scheduled<D: Dispatchable>(
-        &self,
-        portal_id: Self::PortalId,
-        call: D,
-        origin: D::Origin,
-    ) -> Result<DispatchResultWithInfo<D::PostInfo>, Self::Error>;
 
     fn submit_postponed(call: LocalCall, ctx: TransactionCtxId<Self>) -> Result<(), ()>;
 }
--- pallets/deip_portal/src/lib.rs
+++ pallets/deip_portal/src/lib.rs
@@ -25,18 +25,21 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
-mod portal;
+mod module;
 #[cfg(test)]
 mod tests;
 mod transaction_ctx;
 
 pub mod benchmarking;
 pub mod weights;
+mod portal;
 
 #[doc(inline)]
 pub use pallet::*;
 #[doc(inline)]
 pub use portal::*;
+#[doc(inline)]
+pub use module::*;
 #[doc(inline)]
 pub use transaction_ctx::*;
 
@@ -96,16 +99,12 @@ pub mod pallet {
             + From<crate::Call<Self>>
             + UnfilteredDispatchable<Origin = Self::Origin>
             + frame_support::dispatch::Codec
-            + IsSubType<Call<Self>>;
+            + IsSubType<Call<Self>>
+            + TypeInfo;
 
         type UnsignedValidator: ValidateUnsigned<Call = <Self as Config>::Call>;
 
-        type UncheckedExtrinsic: Encode
-            + EncodeLike
-            + Decode
-            + Clone
-            + Debug
-            + Eq
+        type UncheckedExtrinsic: Parameter
             + PartialEq
             + frame_support::traits::ExtrinsicCall
             + Extrinsic<Call = <Self as Config>::Call>
@@ -114,20 +113,36 @@ pub mod pallet {
         type DeipPortalWeightInfo: WeightInfo;
     }
 
+    use frame_support::traits::StorageVersion;
+
+    const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
     #[doc(hidden)]
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
+    #[pallet::storage_version(STORAGE_VERSION)]
     pub struct Pallet<T>(_);
+
+    use frame_support::storage::{StoragePrefixedMap};
+    use sp_runtime::traits::{Hash};
 
     #[doc(hidden)]
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
-        fn offchain_worker(n: BlockNumberFor<T>) {
-            if !sp_io::offchain::is_validator() {
-                debug!("{}", "not a validator");
-                return
+        fn on_runtime_upgrade() -> Weight {
+            if Pallet::<T>::current_storage_version() == V1::<T>::version()
+                && Pallet::<T>::on_chain_storage_version() == V0::<T>::version()
+            {
+                let mut pending: usize = 0;
+                PendingTx::<T>::drain().for_each(|(_, _, xt)| {
+                    let portal_id = ScheduledTx::<T>::take(V0::<T>::extrinsic_hash(&xt)).unwrap();
+                    SignedTx::<T>::insert(V1::<T>::extrinsic_hash(&xt), portal_id);
+                    pending += 1;
+                });
+                let _ = SignedTx::<T>::remove_all(None);
+                return T::DbWeight::get().reads_writes(pending as Weight * 2, pending as Weight + 1);
             }
-            let _ = T::submit_scheduled_tx(n);
+            return 0;
         }
     }
 
@@ -137,7 +152,7 @@ pub mod pallet {
         PortalMismatch,
         AlreadyScheduled,
         UnproperCall,
-        NotScheduled,
+        NotSigned,
         OwnerIsNotATenant,
         PortalAlreadyExist,
         PortalNotFound,
@@ -186,8 +201,8 @@ pub mod pallet {
             origin: OriginFor<T>,
             delegate: PortalDelegate<T>,
             metadata: PortalMetadata,
-        ) -> DispatchResultWithPostInfo {
-            // sp_runtime::runtime_logger::RuntimeLogger::init();
+        ) -> DispatchResultWithPostInfo
+        {
             let owner = ensure_signed(origin)?;
             T::create_portal(owner, delegate, metadata)?;
             Ok(Some(0).into())
@@ -198,8 +213,8 @@ pub mod pallet {
             DispatchClass::Normal,
             Pays::Yes
         ))]
-        pub fn update(origin: OriginFor<T>, update: PortalUpdate<T>) -> DispatchResultWithPostInfo {
-            // sp_runtime::runtime_logger::RuntimeLogger::init();
+        pub fn update(origin: OriginFor<T>, update: PortalUpdate<T>) -> DispatchResultWithPostInfo
+        {
             let owner = ensure_signed(origin)?;
             T::update_portal(owner, update)?;
             Ok(Some(0).into())
@@ -210,13 +225,13 @@ pub mod pallet {
             DispatchClass::Normal,
             Pays::Yes
         ))]
-        pub fn schedule(
+        pub fn sign(
             origin: OriginFor<T>,
             xt: Box<T::UncheckedExtrinsic>,
-        ) -> DispatchResultWithPostInfo {
-            // sp_runtime::runtime_logger::RuntimeLogger::init();
+        ) -> DispatchResultWithPostInfo
+        {
             let delegate = ensure_signed(origin)?;
-            T::schedule_tx(*xt, delegate)?;
+            T::sign_tx(*xt, delegate)?;
             Ok(Some(0).into())
         }
 
@@ -229,9 +244,9 @@ pub mod pallet {
             origin: OriginFor<T>,
             portal_id: PortalId<T>,
             call: Box<<T as Config>::Call>,
-        ) -> DispatchResultWithPostInfo {
-            // sp_runtime::runtime_logger::RuntimeLogger::init();
-            T::dispatch_scheduled_tx(portal_id, *call, origin)?
+        ) -> DispatchResultWithPostInfo
+        {
+            T::exec_signed_tx(portal_id, *call, origin)?
         }
 
         #[pallet::weight((
@@ -243,8 +258,8 @@ pub mod pallet {
             origin: OriginFor<T>,
             portal_id: PortalId<T>,
             call: Box<<T as Config>::Call>,
-        ) -> DispatchResultWithPostInfo {
-            // sp_runtime::runtime_logger::RuntimeLogger::init();
+        ) -> DispatchResultWithPostInfo
+        {
             ensure_none(origin)?;
             T::exec_postponed_tx(portal_id, *call, RawOrigin::None.into())
         }
@@ -255,7 +270,7 @@ pub mod pallet {
     pub type ExtrinsicId = u32;
     pub type ExtrinsicIdList = Vec<ExtrinsicId>;
     pub type PortalInfo<PortalId> = Vec<(PortalId, ExtrinsicIdList)>;
-    pub type ExtrinsicHash = [u8; 32];
+    pub type ExtrinsicHash<T, V> = <V as StorageVersionT<T>>::ExtrinsicHash;
 
     pub fn transpose<'a, T, S, PortalId>(source: S) -> T
     where
@@ -266,19 +281,71 @@ pub mod pallet {
         T::from_iter(source.map(|(x, y)| y.iter().map(move |z| (z, x))).flatten())
     }
 
+    pub trait StorageVersionT<T: Config>: Sized {
+        type ExtrinsicHash: Parameter + Member;
+
+        fn version() -> StorageVersion;
+        fn extrinsic_hash(xt: &T::UncheckedExtrinsic) -> ExtrinsicHash<T, Self> {
+            xt.using_encoded(|b| Self::extrinsic_hash2(b))
+        }
+        fn extrinsic_hash2(xt: &[u8]) -> Self::ExtrinsicHash;
+    }
+
+    pub struct V0<T: Config>(PhantomData<T>) where Self: StorageVersionT<T>;
+
+    impl<T: Config> StorageVersionT<T> for V0<T> {
+        type ExtrinsicHash = [u8; 32];
+
+        fn version() -> StorageVersion {
+            StorageVersion::new(0)
+        }
+        fn extrinsic_hash2(xt: &[u8]) -> ExtrinsicHash<T, Self> {
+            sp_io::hashing::twox_256(&mut &xt)
+        }
+    }
+
+    pub struct V1<T: Config>(PhantomData<T>) where Self: StorageVersionT<T>;
+
+    impl<T: Config> StorageVersionT<T> for V1<T> {
+        type ExtrinsicHash = T::Hash;
+
+        fn version() -> StorageVersion {
+            STORAGE_VERSION
+        }
+        fn extrinsic_hash2(xt: &[u8]) -> ExtrinsicHash<T, Self> {
+            T::Hashing::hash(xt)
+        }
+    }
+
+    // Migration to V2:
+    // Removed
     #[pallet::storage]
     pub(super) type PendingTx<T: Config> = StorageDoubleMap<
         _,
         Twox64Concat,
         T::BlockNumber,
         Blake2_128Concat,
-        ExtrinsicHash,
+        ExtrinsicHash<T, V0<T>>,
         T::UncheckedExtrinsic,
     >;
 
+    // Migration to V2:
+    // Moved to SignedTx
     #[pallet::storage]
-    pub(super) type ScheduledTx<T: Config> =
-        StorageMap<_, Blake2_128Concat, ExtrinsicHash, PortalId<T>>;
+    pub(super) type ScheduledTx<T: Config> = StorageMap<_,
+        Blake2_128Concat,
+        ExtrinsicHash<T, V0<T>>,
+        PortalId<T>
+    >;
+
+    // Migration to V2:
+    // Moved from ScheduledTx
+    #[pallet::storage]
+    pub(super) type SignedTx<T: Config> = StorageMap<_,
+        Identity,
+        ExtrinsicHash<T, V1<T>>,
+        PortalId<T>
+    >;
 
     #[pallet::storage]
     pub(super) type PortalTagOfTransaction<T: Config> = StorageDoubleMap<
@@ -302,40 +369,4 @@ pub mod pallet {
     #[pallet::storage]
     pub(super) type OwnerLookup<T: Config> =
         StorageMap<_, Blake2_128Concat, PortalOwner<T>, PortalId<T>>;
-
-    use sp_core::H256;
-    use storage_ops::*;
-
-    /// Module contains abstractions over pallet storage operations
-    pub mod storage_ops {
-        use sp_std::prelude::*;
-        // use pallet_deip_toolkit::storage_ops::StorageOp;
-        use super::{Config, Pallet};
-        // /// Storage operations
-        // pub enum StorageOps<T: Config> {
-        //     /// Deposit event
-        //     DepositEvent(Event<T>),
-        //     /// Create DAO
-        //     CreateDao(DaoOf<T>),
-        //     /// Update DAO
-        //     UpdateDao(DaoOf<T>),
-        //
-        // }
-        // impl<T: Config> StorageOp for StorageOps<T> {
-        //     fn exec(self) {
-        //         match self {
-        //             Self::DepositEvent(e) => {
-        //                 Pallet::<T>::deposit_event(e)
-        //             },
-        //             Self::CreateDao(dao) => {
-        //                 DaoLookup::<T>::insert(dao.dao_key().clone(), dao.id().clone());
-        //                 DaoRepository::<T>::insert(*dao.id(), dao);
-        //             }
-        //             Self::UpdateDao(dao) => {
-        //                 DaoRepository::<T>::insert(*dao.id(), dao);
-        //             }
-        //         }
-        //     }
-        // }
-    }
 }
