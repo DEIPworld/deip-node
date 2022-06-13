f4b7484565ba12236ce5cfab0d26532214f1c6c4
Pavel Iakushin <jetpilot412@gmail.com>
2022-06-13 14:48:37 +0400

    add CollectionId to config

--- pallets/deip_nft/src/lib.rs
+++ pallets/deip_nft/src/lib.rs
@@ -6,13 +6,15 @@ pub use pallet_uniques;
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{traits::IsType, dispatch::DispatchResult, transactional, sp_runtime::traits::StaticLookup};
+    use frame_support::{traits::IsType, dispatch::DispatchResult, transactional, sp_runtime::traits::StaticLookup, pallet_prelude::Member, Parameter};
     use frame_system::{pallet_prelude::OriginFor, ensure_signed};
 
     #[pallet::config]
     pub trait Config: frame_system::Config + pallet_uniques::Config {
         /// Because this pallet emits events, it depends on the runtime's definition of an event.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+        type CollectionId: Member + Parameter;
     }
 
     #[pallet::pallet]
@@ -29,7 +31,7 @@ pub mod pallet {
     impl<T: Config> Pallet<T> {
         #[pallet::weight(1_000_000)]
         #[transactional]
-        pub fn create_collection(origin: OriginFor<T>) -> DispatchResult {
+        pub fn create_collection(origin: OriginFor<T>, collection: T::CollectionId) -> DispatchResult {
             let issuer = ensure_signed(origin)?;
             let admin = <T::Lookup as StaticLookup>::unlookup(issuer.into());
             
