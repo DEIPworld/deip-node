5cd18ce2cd69089a4c1163f05f4dafa6c39c1dc1
Pavel Iakushin <jetpilot412@gmail.com>
2022-06-17 13:42:49 +0400

    revert formatting

--- deip_common/deip_asset_system/src/ft.rs
+++ deip_common/deip_asset_system/src/ft.rs
@@ -1,21 +1,31 @@
+use sp_runtime::traits::{Zero};
 use crate::FTImplT;
-use sp_runtime::traits::Zero;
 
-pub struct FToken<Impl: FTImplT>
-where
-    Self: FTokenT<Impl>,
-{
+
+pub struct FToken<Impl: FTImplT> where Self: FTokenT<Impl> {
     id: Impl::FTokenId,
     account: Impl::Account,
-    balance: Impl::FTokenAmount,
+    balance: Impl::FTokenAmount
 }
 
-pub trait FTokenT<Impl: FTImplT>: Sized {
-    fn pick_ft(id: Impl::FTokenId, account: &Impl::Account) -> Option<Self>;
+pub trait FTokenT<Impl: FTImplT>: Sized
+{
+    fn pick_ft(
+        id: Impl::FTokenId,
+        account: &Impl::Account
+    ) -> Option<Self>;
 
-    fn transfer_amount(self, to: &Impl::Account, amount: Impl::FTokenAmount) -> Result<(), ()>;
+    fn transfer_amount(
+        self,
+        to: &Impl::Account,
+        amount: Impl::FTokenAmount
+    ) -> Result<(), ()>;
 
-    fn transfer_all(self, to: &Impl::Account) -> Result<(), ()> {
+    fn transfer_all(
+        self,
+        to: &Impl::Account,
+    ) -> Result<(), ()>
+    {
         let all = *self.balance();
         self.transfer_amount(to, all)
     }
@@ -25,23 +35,34 @@ pub trait FTokenT<Impl: FTImplT>: Sized {
     fn balance(&self) -> &Impl::FTokenAmount;
 }
 
-impl<Impl: FTImplT> FTokenT<Impl> for FToken<Impl> {
-    fn pick_ft(id: Impl::FTokenId, account: &Impl::Account) -> Option<Self> {
+impl<Impl: FTImplT> FTokenT<Impl> for FToken<Impl>
+{
+    fn pick_ft(
+        id: Impl::FTokenId,
+        account: &Impl::Account
+    ) -> Option<Self>
+    {
         let balance = Impl::balance(id, account);
-        if balance.is_zero() {
-            return None
-        }
+        if balance.is_zero() { return None }
         Some(Self { id, account: account.clone(), balance })
     }
 
-    fn transfer_amount(self, to: &Impl::Account, amount: Impl::FTokenAmount) -> Result<(), ()> {
+    fn transfer_amount(
+        self,
+        to: &Impl::Account,
+        amount: Impl::FTokenAmount
+    ) -> Result<(), ()>
+    {
         let Self { id, account, balance } = self;
 
-        if amount > balance {
-            return Err(())
-        }
+        if amount > balance { return Err(()) }
 
-        Impl::transfer(id, &account, to, amount).map_err(|_| ())
+        Impl::transfer(
+            id,
+            &account,
+            to,
+            amount
+        ).map_err(|_| ())
     }
 
     fn account(&self) -> &Impl::Account {
