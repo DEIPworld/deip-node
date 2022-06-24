e7a9f66453464caec6355d6a1dce5733491eace1
Pavel Iakushin <jetpilot412@gmail.com>
2022-06-24 12:07:04 +0400

    impl Inspect

--- pallets/deip_nft/src/impl_fungibles.rs
+++ pallets/deip_nft/src/impl_fungibles.rs
@@ -8,72 +8,64 @@ use frame_support::{
     },
 };
 
-use crate::{Config, FractionAmountOf, Pallet};
+use crate::{AssetIdOf, Config, FractionAmountOf, Pallet};
+
+type Assets<T> = pallet_assets::Pallet<T>;
 
 impl<T: Config> Inspect<T::AccountId> for Pallet<T> {
-    type AssetId = T::Hash;
+    type AssetId = AssetIdOf<T>;
 
     type Balance = FractionAmountOf<T>;
 
-    fn total_issuance(item: Self::AssetId) -> Self::Balance {
-        //@TODO rename total_fraction -> total_fraction_issuance.
-        let asset = todo!();
-        total_fraction::<Self>(asset).unwrap_or_else(Zero::zero)
+    fn total_issuance(asset: Self::AssetId) -> Self::Balance {
+        Assets::<T>::total_issuance(asset)
     }
 
-    fn minimum_balance(item: Self::AssetId) -> Self::Balance {
-        let asset = todo!();
-        // @TODO add getter for ft id to public api.
-        <pallet_assets::Pallet<T> as Inspect<T::AccountId>>::minimum_balance(asset)
+    fn minimum_balance(asset: Self::AssetId) -> Self::Balance {
+        Assets::<T>::minimum_balance(asset)
     }
 
-    fn balance(item: Self::AssetId, who: &T::AccountId) -> Self::Balance {
-        let fraction = pick_fraction::<Self>(who, item).unwrap();
-        let fractional_whatever_that_means = fraction.fractional();
-        fractional_whatever_that_means.1
+    fn balance(asset: Self::AssetId, who: &T::AccountId) -> Self::Balance {
+        Assets::<T>::balance(asset, who)
     }
 
     fn reducible_balance(
-        item: Self::AssetId,
+        asset: Self::AssetId,
         who: &T::AccountId,
         keep_alive: bool,
     ) -> Self::Balance {
-        let asset = todo!();
-        // @TODO add getter for ft id to public api.
-        <pallet_assets::Pallet<T> as Inspect<T::AccountId>>::reducible_balance(
-            asset, who, keep_alive,
-        )
+        Assets::<T>::reducible_balance(asset, who, keep_alive)
     }
 
     fn can_deposit(
-        item: Self::AssetId,
+        asset: Self::AssetId,
         who: &T::AccountId,
         amount: Self::Balance,
     ) -> DepositConsequence {
-        let asset = todo!();
-        // @TODO add getter for ft id to public api.
-        <pallet_assets::Pallet<T> as Inspect<T::AccountId>>::can_deposit(asset, who, amount)
+        Assets::<T>::can_deposit(asset, who, amount)
     }
 
     fn can_withdraw(
-        item: Self::AssetId,
+        asset: Self::AssetId,
         who: &T::AccountId,
         amount: Self::Balance,
     ) -> WithdrawConsequence<Self::Balance> {
-        let asset = todo!();
-        // @TODO add getter for ft id to public api.
-        <pallet_assets::Pallet<T> as Inspect<T::AccountId>>::can_withdraw(asset, who, amount)
+        Assets::<T>::can_withdraw(asset, who, amount)
     }
 }
 
 impl<T: Config> Mutate<T::AccountId> for Pallet<T> {
-    fn mint_into(item: Self::AssetId, who: &T::AccountId, amount: Self::Balance) -> DispatchResult {
+    fn mint_into(
+        asset: Self::AssetId,
+        who: &T::AccountId,
+        amount: Self::Balance,
+    ) -> DispatchResult {
         // @TODO add mint_fraction to api.
         todo!()
     }
 
     fn burn_from(
-        item: Self::AssetId,
+        asset: Self::AssetId,
         who: &T::AccountId,
         amount: Self::Balance,
     ) -> Result<Self::Balance, DispatchError> {
